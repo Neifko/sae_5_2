361dab0a92412a60d1655490032449a42bc73d84
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2025-01-06 15:32:10 +0100

    main de test avec des noeuds desactivés pour A*

--- sae_5_2/__main__.py
+++ sae_5_2/__main__.py
@@ -1,6 +1,8 @@
 from sae_5_2.models.Grid import Grid
 from sae_5_2.models.AEtoile import AEtoile
 
+import random
+
 def main():
     # Crée une grille de dimensions 5x5
     width = 10
@@ -10,11 +12,34 @@ def main():
     # Affiche la grille sous forme de matrice
     grid.display_grid()
 
+    # Désactive certains noeuds de manière aléatoire
+    num_nodes_to_deactivate = 7
+    deactivated_nodes = []
+    for _ in range(num_nodes_to_deactivate):
+        while True:
+            x = random.randint(0, width - 1)
+            y = random.randint(0, height - 1)
+            z = -x - y
+            node = grid.get_node(x, y, z)
+            if node and node.active:
+                node.active = False
+                deactivated_nodes.append(node)
+                break
+
+    # Affiche les noeuds désactivés
+    print("\nNoeuds désactivés (coordonnées x, y, z) :")
+    for node in deactivated_nodes:
+        print(f"({node.x}, {node.y}, {node.z})")
+
+
     # Teste l'algorithme A* pour trouver le chemin le plus court
     a_star_solver = AEtoile(grid)
     start = (0, 0, 0)
     goal = (2, 5, -7)
     path = a_star_solver.a_star(start, goal)
+
+    print("\nDépart :", start)
+    print("Arrivée :", goal)
 
     if path:
         print("\nChemin trouvé :")
