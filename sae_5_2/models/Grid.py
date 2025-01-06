a42cd70a1d97e0941b8da03495be6d19330879f3
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2025-01-06 14:23:45 +0100

    Adding some comments

--- sae_5_2/models/Grid.py
+++ sae_5_2/models/Grid.py
@@ -3,13 +3,16 @@ from sae_5_2.models.Node import Node
 
 class Grid:
     """
-    Classe représentant une grille hexagonale et son graphe.
-    La grille est générée en utilisant des coordonnées cubiques.
+    Classe représentant une grille contenant des noeuds hexagonaux.
+    La grille est générée en utilisant des coordonnées cubiques (pour les déplacements qui seront fait par les algorithmes).
     """
-    def __init__(self, rows, cols):
-        self.rows = rows  # Nombre de lignes dans la grille
-        self.cols = cols  # Nombre de colonnes dans la grille
-        self.nodes = {}    # Dictionnaire pour stocker les nœuds
+    def __init__(self, rows:int, cols:int):
+        """
+        Constructeur de la classe Grid qui prend en paramètre le nombre de lignes et de colonnes.
+        """
+        self.rows = rows  
+        self.cols = cols  
+        self.nodes = {}           # Dictionnaire pour stocker les nœuds
         self.directions = {
             "N": (+1, -1, 0),     # Nord
             "NE": (+1, 0, -1),    # Nord-Est
@@ -21,7 +24,9 @@ class Grid:
         self._create_grid()
 
     def _create_grid(self):
-        """Crée la grille avec des nœuds hexagonaux"""
+        """
+        Méthode qui permet d'ajouter les noeuds de la grille en les créants et en les connectants entre eux.
+        """
         for x in range(self.cols):
             for y in range(self.rows):
                 z = -x - y  # Calcul de z pour respecter x + y + z = 0
@@ -35,7 +40,9 @@ class Grid:
                     node.voisins[direction] = self.nodes[neighbor_coords]
 
     def display_grid(self):
-        """Affiche graphiquement la grille hexagonale avec les coordonnées"""
+        """
+        Affiche la grille en ligne de commande avec les coordonnées cubiques de chaque nœud.  
+        """
         for y in range(self.rows):
             line = ""
             for x in range(self.cols):
@@ -46,8 +53,10 @@ class Grid:
                     line += f"   ({x},{y},{-x-y}) "
             print(line)
 
-    def display_neighbors(self, x, y, z):
-        """Affiche les voisins d'un nœud spécifique avec leurs directions cardinales"""
+    def display_neighbors(self, x:int, y:int, z:int):
+        """
+        Affiche les voisins d'un nœud en précisant ses coordonées (x, y, z).
+        """
         node = self.get_node(x, y, z)
         if not node:
             print(f"Pas de nœud trouvé aux coordonnées ({x}, {y}, {z}).")
@@ -58,6 +67,8 @@ class Grid:
         for direction, neighbor in node.voisins.items():
             print(f"  Direction {direction}: {neighbor}")
 
-    def get_node(self, x, y, z):
-        """Retourne le nœud aux coordonnées (x, y, z)."""
+    def get_node(self, x:int, y:int, z:int):
+        """
+        Retourne le nœud aux coordonnées (x, y, z).
+        """
         return self.nodes.get((x, y, z))
