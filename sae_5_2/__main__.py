0bacadc5c1b88b4c793bbdd9c3f62a2d4a9ad4ee
Victor G <20vg01@gmail.com>
2025-01-07 09:41:08 +0100

    deplacement fonctionnement profondeur

--- sae_5_2/__main__.py
+++ sae_5_2/__main__.py
@@ -13,29 +13,4 @@ if __name__ == "__main__":
     # # Afficher les voisins d'un nœud particulier
     # hex_grid.display_neighbors(0, 1, -1)  # Coordonnées (x=2, y=2, z=-4)
 
-    # Créer une grille de taille 5x5
-    hex_grid = Grid(3, 3)
-    hex_grid.display_grid()
-
-    # Initialiser le parcours en profondeur récursif (DFS)
-    parcours_profondeur = ParcoursProfondeur(hex_grid)
-
-    # Définir le point de départ et d'arrivée
-    start_coords = (0, 0, 0)
-    target_coords = (2, 2, -4)
-
-    hex_grid.display_neighbors(*start_coords)
-    hex_grid.display_neighbors(*target_coords)
-
-
-
-    # Lancer DFS récursif
-    path = parcours_profondeur.parcours(start_coords, target_coords)
-
-    # Afficher les résultats
-    if path:
-        print(f"Un chemin existe entre {start_coords} et {target_coords}.")
-        # print(f"Distance totale : {distance}")
-        print(f"Chemin trouvé : {path}")
-    else:
-        print(f"Aucun chemin trouvé entre {start_coords} et {target_coords}.")
+    print("FAIT")
