18b0aaef831510419630d623427845f360d41e84
Victor G <20vg01@gmail.com>
2025-01-07 09:41:08 +0100

    deplacement fonctionnement profondeur

---
+++ sae_5_2/controlers/ProfondeurController.py
@@ -0,0 +1,30 @@
+from sae_5_2.models.Grid import Grid
+from sae_5_2.models.ParcoursProfondeur import ParcoursProfondeur
+
+
+class ProfondeurController:
+    def display_profondeur_in_console(self):
+        # Créer une grille de taille 5x5
+        hex_grid = Grid(3, 3)
+        hex_grid.display_grid()
+
+        # Initialiser le parcours en profondeur récursif (DFS)
+        parcours_profondeur = ParcoursProfondeur(hex_grid)
+
+        # Définir le point de départ et d'arrivée
+        start_coords = (0, 0, 0)
+        target_coords = (2, 2, -4)
+
+        hex_grid.display_neighbors(*start_coords)
+        hex_grid.display_neighbors(*target_coords)
+
+        # Lancer DFS récursif
+        path = parcours_profondeur.parcours(start_coords, target_coords)
+
+        # Afficher les résultats
+        if path:
+            print(f"Un chemin existe entre {start_coords} et {target_coords}.")
+            # print(f"Distance totale : {distance}")
+            print(f"Chemin trouvé : {path}")
+        else:
+            print(f"Aucun chemin trouvé entre {start_coords} et {target_coords}.")
