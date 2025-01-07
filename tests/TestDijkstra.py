f73a4e675851898c1bda19608af56e2adabf890c
Junior 78 <alexie78180@gmail.com>
2025-01-07 15:09:42 +0100

    feat(2): fin test Dijkstra

--- tests/TestDijkstra.py
+++ tests/TestDijkstra.py
@@ -17,7 +17,7 @@ class TestDijkstra(unittest.TestCase):
             (0, 0, 0),
             (1, -1, 0)
         )
-        print("\nTest 1: Chemin simple")
+        print("\ntest_shortest_path_adjacent: Chemin simple")
         print("Nombre de déplacement :", distance)
         print("Chemin trouvé :", path)
         self.assertEqual(distance, 1)
@@ -36,13 +36,13 @@ class TestDijkstra(unittest.TestCase):
         self.assertEqual(len(path), 3)
 
     def test_invalid_coordinates(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError, msg="Le chemin n'est pas possible si la case est inexistante"):
             self.dijkstra.shortest_path(
                 self.grid,
                 (10, 10, -20),
                 (0, 0, 0)
             )
-            print("\nTest 3: Aucun chemin possible")
+        print("\ntest_invalid_coordinates: Aucun chemin possible")
 
 
     def test_path_to_self(self):
@@ -51,28 +51,11 @@ class TestDijkstra(unittest.TestCase):
             (0, 0, 0),
             (0, 0, 0)
         )
-        print("\nTest 2: Départ et arrivée identiques")
+        print("\ntest_path_to_self: Départ et arrivée identiques")
         print("Nombre de déplacement :", distance)
         print("Chemin trouvé :", path)
         self.assertEqual(distance, 0)
         self.assertEqual(len(path), 1)
-
-    def test_3(self):
-        start = self.grid.get_node(0, 0, 0)
-        goal = self.grid.get_node(5, 5, -10)
-        path = self.dijkstra.shortest_path(self.grid, *start, *goal)
-        print("\nTest 3: Aucun chemin possible")
-        print("Chemin trouvé :", path)
-        self.assertListEqual(path, [], "Le chemin doit être None si aucun chemin n'est possible")
-
-    def test_4(self):
-        start = self.grid.get_node(-2, -2, 4)
-        goal = self.grid.get_node(2, 2, -4)
-        path = self.dijkstra.shortest_path(self.grid, start, goal)
-        print("\nTest 4: Plus grand chemin")
-        print("Chemin trouvé :", path)
-        self.assertIsNotNone(path, "Le chemin ne doit pas être None")
-        self.assertGreater(len(path), 0, "Le chemin doit contenir au moins un nœud")
 
 if __name__ == '__main__':
     unittest.main()
